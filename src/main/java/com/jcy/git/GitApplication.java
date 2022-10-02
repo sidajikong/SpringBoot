eab689c7cf5f997fe65ddb6580de8197d4d9a2c1
Jiang <jcy@163.com>
2022-10-02 16:57:31 +0800

    hot-fix second commit

--- src/main/java/com/jcy/git/GitApplication.java
+++ src/main/java/com/jcy/git/GitApplication.java
@@ -11,6 +11,7 @@ public class GitApplication {
         System.out.println("hello git!");
         System.out.println("hello git!!");
         System.out.println("hello git!!!");
+        System.out.println("hello git!!!!");
     }
 
 }
